e68bcb6c14907ec14376435efe60dff7fce3fce6
Adam Shimali <adam.shimali@gmail.com>
2015-05-06 14:18:57 +0100

    remove comments

--- {{cookiecutter.application_name}}/{{cookiecutter.application_name}}/frontend/views.py
+++ {{cookiecutter.application_name}}/{{cookiecutter.application_name}}/frontend/views.py
@@ -3,10 +3,6 @@ from flask import (
     render_template
 )
 
-# we can get to the registry/registries configured using:
-# from {{cookiecutter.application_name}} import app
-# then use : app.register ... to get/put etc.
-
 frontend = Blueprint('frontend', __name__, template_folder='templates')
 
 @frontend.route('/')
