619a47542912c6ccf816e0562023f8cb376ed12e
Adam Shimali <adam.shimali@gmail.com>
2015-05-06 14:15:52 +0100

    get port from env var

--- {{cookiecutter.application_name}}/manage.py
+++ {{cookiecutter.application_name}}/manage.py
@@ -5,9 +5,10 @@ from flask.ext.script import Shell, Server, Manager
 
 from {{cookiecutter.application_name}} import app
 
+port = os.environ.get('PORT', 8000)
+
 manager = Manager(app)
-manager.add_command('server', Server())
+manager.add_command('server', Server(host="0.0.0.0", port=port))
 
 if __name__ == '__main__':
     manager.run()
-
