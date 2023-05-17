84a906193a11b9809c8bcfcba49c676dda9467bb
Adam Shimali <adam.shimali@gmail.com>
2023-05-17 17:09:09 +0100

    bit more reorganisation

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -10,5 +10,5 @@ python -m pip install pip-tools
 python -m piptools compile requirements/requirements.in
 python -m piptools compile requirements/dev-requirements.in
 python -m piptools sync requirements/requirements.txt requirements/dev-requirements.txt
-flask db init
+# flask db init
 # python -m pre_commit install
