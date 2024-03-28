5fd6f5fda8486e2f45d8e8bb05d7fcf5a08fe3df
Adam Shimali <adam.shimali@gmail.com>
2024-03-28 15:17:37 +0000

    include pre commit install

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -5,11 +5,10 @@ echo "Running post gen project script"
 uv venv {{cookiecutter.application_name}}
 source {{cookiecutter.application_name}}/bin/activate
 
-
 uv pip install --upgrade pip
 uv pip compile requirements/requirements.in  -o requirements/requirements.txt
 uv pip compile requirements/dev-requirements.in  -o requirements/dev-requirements.txt
 uv pip sync requirements/requirements.txt
 uv pip sync requirements/dev-requirements.txt
 
-# python -m pre_commit install
+pre_commit install
